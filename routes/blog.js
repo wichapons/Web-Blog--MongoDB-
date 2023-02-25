d30660925d4be9b45dbffa6248e84c9eac8de086
Wichapon V <wichapon@gmail.com>
2023-02-25 22:27:42 +0700

    Add post form: update action and select author value

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('../data/database');
 const router = express.Router();
+const mongodb = require('mongodb');
 
 router.get('/', function(req, res) {
   res.redirect('/posts');
@@ -13,6 +14,27 @@ router.get('/posts', function(req, res) {
 router.get('/new-post', async function(req, res) {
   let authorsData = await db.getDB().collection('authors').find().toArray(); //the main difference between with or without toArray is that the first one retrieves the result set as an array using the toArray() method, while the second one returns a cursor object that you can use to iterate over the result set and perform operations on it. Which one to use depends on your specific use case and what you want to do with the result set.
   res.render('create-post',{authors:authorsData});
+});
+
+router.post('/post', async function(req, res) {
+  let postID = req.params.id;
+  let postDetails = req.body;
+
+  let authorID = new mongodb.ObjectId(postDetails.author) //convert Author ID that's sent from the form which is STRING to ObjectID to make MongoDB understand  
+  let author = await db.getDB().collection('authors').findOne({_id:authorID});
+  let newPost = {
+    title:postDetails.title,
+    summary: postDetails.summary,
+    body: postDetails.content,
+    date: new Date(),
+    author:{
+      id: authorID,
+      name:author.name
+    }
+  }
+  
+  let authorsData = await db.getDB().collection('posts').insertOne(newPost); //the main difference between with or without toArray is that the first one retrieves the result set as an array using the toArray() method, while the second one returns a cursor object that you can use to iterate over the result set and perform operations on it. Which one to use depends on your specific use case and what you want to do with the result set.
+  res.redirect('/');
 });
 
 module.exports = router;
