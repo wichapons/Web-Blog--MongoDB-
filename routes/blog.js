e5cdd921e8706eb67b481ad475ef461d42ec340d
Wichapon V <wichapon@gmail.com>
2023-02-25 13:31:30 +0700

    Add authors data to create-post view

--- routes/blog.js
+++ routes/blog.js
@@ -12,7 +12,7 @@ router.get('/posts', function(req, res) {
 
 router.get('/new-post', async function(req, res) {
   let authorsData = await db.getDB().collection('authors').find().toArray(); //the main difference between with or without toArray is that the first one retrieves the result set as an array using the toArray() method, while the second one returns a cursor object that you can use to iterate over the result set and perform operations on it. Which one to use depends on your specific use case and what you want to do with the result set.
-  res.render('create-post');
+  res.render('create-post',{authors:authorsData});
 });
 
 module.exports = router;
