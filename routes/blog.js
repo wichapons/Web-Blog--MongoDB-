221316d0b7a717cfe515078c6b377240d4146671
Wichapon V <wichapon@gmail.com>
2023-02-26 12:43:00 +0700

    Add route and view to view post details

--- routes/blog.js
+++ routes/blog.js
@@ -9,8 +9,17 @@ router.get('/', function(req, res) {
 
 router.get('/posts', async function(req, res) {
   const postDetails = await db.getDB().collection('posts').find({},{title:1,summary:1,'author.name':1}).toArray();
-  
   res.render('posts-list',{posts:postDetails});
+});
+
+router.get('/view/:id', async function(req, res) {
+  let postID = req.params.id;
+  console.log(postID);
+  //let authorID = new mongodb.ObjectId('postID')
+  let postDetails = await db.getDB().collection('posts').find({},{_id:postID}).toArray();
+  console.log(postDetails);
+  //const postDetails = await db.getDB().collection('posts').find({},{title:1,summary:1,'author.name':1}).toArray();
+  res.render('post-detail',{posts:postDetails});
 });
 
 router.get('/new-post', async function(req, res) {
@@ -19,7 +28,7 @@ router.get('/new-post', async function(req, res) {
 });
 
 router.post('/post', async function(req, res) {
-  let postID = req.params.id;
+  
   let postDetails = req.body;
 
   let authorID = new mongodb.ObjectId(postDetails.author) //convert Author ID that's sent from the form which is STRING to ObjectID to make MongoDB understand  
@@ -34,9 +43,11 @@ router.post('/post', async function(req, res) {
       name:author.name
     }
   }
-  
-  let authorsData = await db.getDB().collection('posts').insertOne(newPost); //the main difference between with or without toArray is that the first one retrieves the result set as an array using the toArray() method, while the second one returns a cursor object that you can use to iterate over the result set and perform operations on it. Which one to use depends on your specific use case and what you want to do with the result set.
+  await db.getDB().collection('posts').insertOne(newPost); //the main difference between with or without toArray is that the first one retrieves the result set as an array using the toArray() method, while the second one returns a cursor object that you can use to iterate over the result set and perform operations on it. Which one to use depends on your specific use case and what you want to do with the result set.
   res.redirect('/');
 });
+
+
+
 
 module.exports = router;
